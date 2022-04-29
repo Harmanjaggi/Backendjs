b901ed13390f6e730302e7754b8168d14abebbdb
Harman <51782270+Harmanjaggi@users.noreply.github.com>
2022-04-29 15:02:23 +0530

    final

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
+var cors = require('cors')
 const app = express();
 const { update } = require('lodash');
 const cookieParser = require('cookie-parser');
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-app.listen(3000);   
+const port = process.env.port || 5000;
+app.listen(port, function () {
+    console.log('server listening on port' + port);
+});   
 
 // mini app
 const userRouter = require('./routers/userRouter');
